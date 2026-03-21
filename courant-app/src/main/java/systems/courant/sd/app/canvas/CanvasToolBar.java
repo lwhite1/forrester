18d0bae9cb1ea1e70a0f99326840ff0afb843219
Larry White <lwhite1@users.noreply.github.com>
2026-03-21 12:41:23 -0400

    Move info link button to stock-and-flow toolbar group
    Info link operates on module ports, which are stock-and-flow
    elements. Move it out of the causal diagram group to sit after
    the module button where it logically belongs.

--- courant-app/src/main/java/systems/courant/sd/app/canvas/CanvasToolBar.java
+++ courant-app/src/main/java/systems/courant/sd/app/canvas/CanvasToolBar.java
@@ -142,8 +142,8 @@ public class CanvasToolBar extends ToolBar {
         });
 
         getItems().addAll(selectButton, new Separator(),
-                stockButton, flowButton, auxButton, lookupButton, moduleButton,
-                new Separator(), cldVarButton, causalLinkButton, infoLinkButton,
+                stockButton, flowButton, auxButton, lookupButton, moduleButton, infoLinkButton,
+                new Separator(), cldVarButton, causalLinkButton,
                 new Separator(), commentButton,
                 new Separator(), loopsButton, new Separator(), validateButton,
                 spacer, searchHint);
